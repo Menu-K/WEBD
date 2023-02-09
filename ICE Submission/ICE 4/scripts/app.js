b9302e98912ea3bdeda4411f6928933e2f19e233
Menu-K <menushan.karunakaran@gmail.com>
2023-02-09 14:46:39 -0500

    ice 4 completion/ starting lab 2

--- ICE Submission/ICE 4/scripts/app.js
+++ ICE Submission/ICE 4/scripts/app.js
@@ -1,6 +1,8 @@
 console.log('app.js loaded')
 // TO DO: load the user class script with alias
-
+// import * as userClass from "./user.js"
+// let newUser = new userClass.User("a", "b", "c", "d", "e");
+// console.log('newUser.displayUser());
 
 
 /**
@@ -17,7 +19,7 @@ $(function () {
                 </div>`
 
     //TO DO: replace the nav-holder
-
+    $("#nav-holder").replaceWith(navBar);
 });
 
 /**
@@ -31,146 +33,174 @@ $(function () {
 /**
  * fuction to demo adding content with .text() and .html()
  *  */
-// $(function () {
+$(function () {
 
     // let navDiv = $("div:first");
     // console.log(navDiv.html())
     // console.log(navDiv.text())
 
-    // let contentDiv = $("#content-div");
+    let contentDiv = $("#content-div");
     // console.log(contentDiv.html())
     // console.log(contentDiv.text())
 
     // text to use with new paragraph
-    // text = "It is one of his best works!"
+    let text = "It is one of his best works!"
     // create a paragraph
-    // let newP = $("<p></p>");
+    let newP = $("<p></p>");
     // TO DO: add text with html()
-
+    newP.text(text);
     // append to contentDiv
+    // contentDiv.append(newP);
+    newP.appendTo(contentDiv);
 
     // add text
-    // newP.text("I am very excited for the opening of the new adaptation of his work.");
+    newP.text("I am very excited for the opening of the new adaptation of his work.");
+
     // TO DO: test when very excited is in a strong tag
+    // newP.text("I am <strong> very excited </strong> for the opening of the new adaptation of his work.");
 
     // TO DO: append to newP text
-
-// });
+    newP.html("I am <strong> very excited </strong> for the opening of the new adaptation of his work.");
+});
 
 /**
  * function to demo adding toggle to button
  *  */
-// $(function () {
+$(function () {
     // TO DO: complete the function
     // get the button
+    let btnHide = $("#btnDivToggle");
+
     // add a click function
+    btnHide.click(function () {
         // get the parent div's p tags
+        let paras = $(".toggleDiv").find("p");
+
         // for each p in the div
-            // if it has toggleHide class
-                //remove toggleHide class and add toggleShow class
-                // styling is controlled in the css
-            // otherwise assume it has the toggleShow class
-                //remove toggleShow and add toggleHide
+        $(paras).each(function () {
+            $(this).toggle();
+            // // if it has toggleHide class
+            // if ($(this).hasClass("toggleHide")) {
+            //     // remove toggleHide class and add toggleShow class
+            //     // styling is controlled in the css
+            //     $(this).removeClass("toggleHide").addClass("toggleShow");
+            // }
+            // else
+            // {
+            //     // otherwise assume it has the toggleHide class
+            //     //remove toggleHide and add toggleShow
+            //     $(this).removeClass("toggleShow").addClass("toggleHide");
+            // }
+        });
+    });
+});
 
-//  });
-
-
+// function to add the image next to the text
+if ($("speckled-excerpt")) {
+    let aspImg = "<img class='asp-img' src='./images/asp.jpeg'>";
+    $(aspImg).insertBefore($("#speckled-excerpt"));
+}
 
 // FORM JQUERY
 // TO DO: import form validation functions with alias
 
 // if the submit button is on the page
 // if ($("#btnRegSubmit")) {
-    // TO DO: add a click function that calls a callack function
-    // $("#btnRegSubmit").click(function (e) {
-        // prevent the default submit action (stay on the page)
+// TO DO: add a click function that calls a callack function
+// $("#btnRegSubmit").click(function (e) {
+// prevent the default submit action (stay on the page)
 
-        // create a new user
-        // you normally wouldn't do this unless you had validated, but we're going to do it to show how class memebers work in calling the validation
+// create a new user
+// you normally wouldn't do this unless you had validated, but we're going to do it to show how class memebers work in calling the validation
 
-            // get the first name input
+// get the first name input
 
-            // get the last name input
+// get the last name input
 
-            // get the username input
+// get the username input
 
-            // get the email input
+// get the email input
 
-            // get the password input
+// get the password input
 
 
 
-        // debug statement for object
-        // console.log(`UserDetails: ${user.displayUser()}`)
+// debug statement for object
+// console.log(`UserDetails: ${user.displayUser()}`)
 
-        // validate first name
+// validate first name
 
-        // validate last name
+// validate last name
 
-        // validate  username
+// validate  username
 
-        // validate confirm password
+// validate confirm password
 
 // });
 // }
 
 // TO DO: if reset button present
 
-    // bind a click event handler
+// bind a click event handler
 
-        // clear out all error message paragraphs
+// clear out all error message paragraphs
 
 
 
 
 // SLIDESHOW
 // TO DO: if there's a gallery class on the page
-
-    // call a callback function to handle the galler rotation
-
+if ($(".gallery")) {
+    $(function () {
+        // call a callback function to handle the galler rotation
 
         // get the image tag
+        let galleryImage = $(".gallery").find("img").first();
         // get a list of your images
-        // let images = [
-        //     "./images/portraits/portrait-01.jpg",
-        //     "./images/portraits/portrait-02.jpg",
-        //     "./images/portraits/portrait-03.jpg",
-        //     "./images/portraits/portrait-04.jpg",
-        //     "./images/portraits/portrait-05.jpg",
-        //     "./images/portraits/portrait-06.jpg",
-        //     "./images/portraits/portrait-07.jpg",
-        //     "./images/portraits/portrait-08.jpg",
-        //     "./images/portraits/portrait-09.jpg",
-        //     "./images/portraits/portrait-10.jpg",
-        //     "./images/portraits/portrait-11.jpg",
-        //     "./images/portraits/portrait-12.jpg",
-        //     "./images/portraits/portrait-13.jpg",
-        //     "./images/portraits/portrait-14.jpg",
-        //     "./images/portraits/portrait-15.jpg",
-        //     "./images/portraits/portrait-16.jpg",
-        //     "./images/portraits/portrait-17.jpg",
-        //     "./images/portraits/portrait-18.jpg",
-
-        // ]
-
-
+        let images = [
+            "./images/portraits/portrait-01.jpg",
+            "./images/portraits/portrait-02.jpg",
+            "./images/portraits/portrait-03.jpg",
+            "./images/portraits/portrait-04.jpg",
+            "./images/portraits/portrait-05.jpg",
+            "./images/portraits/portrait-06.jpg",
+            "./images/portraits/portrait-07.jpg",
+            "./images/portraits/portrait-08.jpg",
+            "./images/portraits/portrait-09.jpg",
+            "./images/portraits/portrait-10.jpg",
+            "./images/portraits/portrait-11.jpg",
+            "./images/portraits/portrait-12.jpg",
+            "./images/portraits/portrait-13.jpg",
+            "./images/portraits/portrait-14.jpg",
+            "./images/portraits/portrait-15.jpg",
+            "./images/portraits/portrait-16.jpg",
+            "./images/portraits/portrait-17.jpg",
+            "./images/portraits/portrait-18.jpg",
+        ]
 
         // set a first index
-
+        let imageIndex = 0;
         // call the setInterval method that will re-call this method at a set interval
-
+        setInterval(function () {
             // increment the image index but no greater than how many images you have
+            imageIndex = (imageIndex + 1) % images.length;
 
             // fade out the current image
+            galleryImage.fadeOut(1000, function () {
 
                 // $(this) refers to the object that calls the callback or in this case galleryImage
                 // change the src attribute of the image
-
+                $(this).attr("src", images[imageIndex]);
                 // fade it back in
-
+                $(this).fadeIn(1000);
                 // debug statement
-
+                console.log($(this).attr("src"));
+            })
             //set the time for more than how long the fade out and in process will take or you won't get the images you expect
+        }, 3000);
+    });
+};
+
 
 
 
---
+++ Lab 2/scripts/app.js
@@ -0,0 +1,288 @@
+/*
+Author: [Menushan Karunakaran]
+Student ID: [100709847]
+Date Completed: [2023-02-02]
+
+Image Courtesy to unsplash.com
+Images relating to my interest, was taken from the web
+*/
+
+(function () {
+    // The DisplayHomePage function is called when the home page loads
+    function displayHomePage() {
+        console.log("Home Page"); // log to the console that the home page has loaded
+
+        // Get the About Us button element by its id
+        let AboutUsButton = document.getElementById("AboutUsButton");
+
+        // Add a click event listener to the About Us button
+        AboutUsButton.addEventListener("click", function () {
+            // When the About Us button is clicked, navigate to the about.html page
+            location.href = "about.html";
+        });
+
+        // Get the element with id "fun-fact" and set its inner HTML to the fun fact message
+        document.getElementById("fun-fact").innerHTML =
+            "Fun Fact: This is from Sri Lanka [Nuwara Eliya]";
+    }
+
+    // This function creates a product section and displays the information of each product in it.
+    function displayProductsPage()
+    {
+        // Get the Products link
+        var productsLink = document.getElementById("products");
+
+        // Change the text of the Products link to Interests
+        productsLink.innerHTML = "Interests";
+
+        // An array that holds the information of each product.
+        const products =[
+            {
+                title: "Harry Potter",
+                description: ` Harry Potter is a series of seven fantasy novels written by British author J.K. Rowling. The story follows a young wizard, Harry Potter, and his friends Hermione Granger and Ron Weasley, who are students at Hogwarts School of Witchcraft and Wizardry. Together, they explore the magical world, fight against the evil Lord Voldemort and his followers, the Death Eaters, and uncover the truth about Harry's past. The series has become one of the best-selling book series in history and has been adapted into a successful film franchise.`,
+                image: "./images/harryPotter.jpg"
+
+            },
+            {
+                title: "The Last of Us",
+                description: `The Last of Us is a post-apocalyptic action-adventure game developed by Naughty Dog. The game follows Joel and Ellie, two survivors in a world that has been devastated by a fungal infection that has turned most of the human population into aggressive creatures. As they journey across the country, they must rely on each other to survive and overcome various obstacles, both human and infected. The game received widespread critical acclaim for its story, characters, and gameplay, and has since been remastered for newer consoles.`,
+                image: "./images/lastOfUs.jpg",
+            },
+            {
+                title: "Only Murders in the Building",
+                description: `Only Murders in the Building is an American mystery comedy-drama television series created by Steve Martin, John Strauss, and Martin Short. The series follows three strangers who share an obsession with true crime and suddenly find themselves wrapped up in one. The unlikely trio of amateur sleuths navigate their way through the twists and turns of the murder investigation, using their unique skills and knowledge to try to solve the crime. The series is a witty and entertaining take on the classic murder mystery genre.`,
+                image: "./images/murderInTheBuilding.jpg",
+            },
+        ];
+
+        // Select the product section in the HTML file
+        const productSection = document.querySelector("#product-section");
+
+        // Loop through the products array
+        for (let i = 0; i < products.length; i++)
+        {
+            let product = products[i];
+
+            // Append each product to the product section
+            productSection.innerHTML += `
+            <div class="product">
+            <h2>${product.title}</h2>
+            <p>${product.description}</p>
+            <img src="${product.image}" alt="${product.title}">
+            </div>
+            `;
+        }
+    }
+
+    // DisplayServicesPage() - A function that displays information about the services
+    function displayServicesPage() {
+        console.log("Services Page");
+
+        // Array of service objects with title, description and image properties
+        const services = [
+            {
+                title: "Web Design",
+                description:
+                    "I offer professional web design services to help clients establish their online presence. My design services range from simple website layouts to complex e-commerce solutions.",
+                image: "./images/webDesign.jpg",
+            },
+            {
+                title: "Skilled in (C++, C#, java)",
+                description:
+                    "I am proficient in several programming languages, including C++, C#, and Java. I can develop software solutions for clients in these languages to meet their specific needs.",
+                image: "./images/language.jpg",
+            },
+            {
+                title: "Custom Programming",
+                description:
+                    "I offer custom programming services to help clients bring their ideas to life. Whether its a desktop application, a mobile app, or a web-based solution, I have the skills and experience to deliver.",
+                image: "./images/customProg.jpg",
+            },
+        ];
+        // Selecting the element with ID "services-container"
+        const servicesContainer = document.querySelector("#services-container");
+
+        // Loop through the services array and add the service title, description, and image to the services container
+        for (let i = 0; i < services.length; i++) {
+            let service = services[i];
+            servicesContainer.innerHTML += `
+        <h2>${service.title}</h2>
+        <p>${service.description}</p>
+        <img src="${service.image}" alt="${service.title}">
+        `;
+        }
+    }
+
+    // displayAboutPage() - A function to display information about the "About Us" page
+    function displayAboutPage() {
+        console.log("About Page");
+        const aboutUsContainer = document.querySelector("#about-us-container");
+
+        // First section HTML
+        const section1 = `
+    <div id="section-1">
+        <div class="images-container">
+            <img src="./images/you.jpg" alt="Image 1">
+            <img src="./images/you2.jpg" alt="Image 2">
+            <img src="./images/pet.jpg" alt="Image 3">
+        </div>
+        <p>My name is Menushan Karunakaran, due to reasons, studying for Computer Programmer Diploma. I live all the way in Markham while I go to school at Durham College, Oshawa. As you can see with the pictures above, 
+         I seem to be in to fashion when I just like to wear clothes that I believe look nice. The other picture you see is my brat of a sister of a dog named "Fanta". I am the only boy of 3 (4 including my dog) girl siblings.
+         I have got a older sister named "Thanusha", a twin sister "Minusha" and a baby sister named "Risza". My parents are Sri Lankan, born there and all of us kids were born here in Canada </p>
+    </div>
+    `;
+
+        // Second section HTML
+        const section2 = `
+    <div id="section-2">
+        <p>As you can see from the logo below, I love soccer and I spent a good part of my life playing it. My favourite sports are Soccer, Volleyball, Badminton, Ultimate frisbee and table tennis. I have the physique for basketball 
+        but I don't like it and despise it. I spent from grade 4 to grade 10 playing for Unionville Milliken Soccer Club for House League, but I also had the opportunity to play in REP(Representative) and train players
+        in that and All Star league. I also love to play video games and use it to relax which is very unhealthy. I am using the term video games vaguely because I love to play all types of games from Minecraft to 
+        horror games to pokemon etc. I grew up with pokemon and have a soft spot with it as well as Mario bros. As I grew older I had the opportunity to play semi pro for CounterStrike Global Offensive (CSGO) and 
+        the Call of Duty franchise. Growing older as come with its drawbacks and I now play games to enjoy myself and I can't keep up with these young'uns (old man joke). </p>
+        <div class="images-container">
+            <img src="./images/UMSC.png" alt="Image 4">
+            <img src="./images/csgo.jpg" alt="Image 5">
+            <img src="./images/fanta.jpg" alt="Image 6">
+        </div>
+    </div>
+    `;
+        // Inject the HTML into the aboutUsContainer element
+        aboutUsContainer.innerHTML = section1 + section2;
+    }
+
+    /**********************************
+                    Contact Form  
+        **********************************/
+    // Function to display contact page
+    function displayContactPage() {
+        console.log("Contact Page");
+        // Event listener for submit button click
+        document
+            .getElementById("submitBtn")
+            .addEventListener("click", function (event) {
+                // Variables to store user inputs
+                let name = document.getElementById("name").value;
+                let contactNumber = document.getElementById("contactNumber").value;
+                let email = document.getElementById("email").value;
+                let message = document.getElementById("message").value;
+
+                // Variables to store error elements
+                let nameError = document.getElementById("nameError");
+                let contactNumberError = document.getElementById("contactNumberError");
+                let emailError = document.getElementById("emailError");
+
+                // Variable to store validation status
+                let isValid = true;
+
+                // Name Validation
+                if (!name) {
+                    nameError.style.display = "block";
+                    isValid = false;
+                } else {
+                    nameError.style.display = "none";
+                }
+                // Contact Number Validation
+                if (isNaN(contactNumber) || contactNumber.length !== 10) {
+                    contactNumberError.style.display = "block";
+                    isValid = false;
+                } else {
+                    contactNumberError.style.display = "none";
+                }
+                // Email Validation
+                if (
+                    !email ||
+                    !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+                ) {
+                    emailError.style.display = "block";
+                    isValid = false;
+                } else {
+                    emailError.style.display = "none";
+                }
+
+                // If all user inputs are valid
+                if (isValid) {
+                    // Output user information to console
+                    console.log("Name: " + document.getElementById("name").value);
+                    console.log("Contact Number: " + contactNumber);
+                    console.log("Email: " + email);
+                    console.log("Message: " + document.getElementById("message").value);
+
+                    // Courtesy to [https://stackoverflow.com/questions/10312963/difference-between-settimeout-with-and-without-quotes-and-parentheses]
+                    // Start 3 second timer
+                    setTimeout(function () {
+                        // Redirect back to Home Page (index.html)
+                        window.location.href = "index.html";
+                    }, 3000);
+                }
+            });
+    }
+
+    // Start function to determine which page is currently loaded
+    function Start() {
+        console.log("App Started!"); // log a message to indicate the start of the app
+
+        // switch statement to determine which page is currently loaded
+        switch (document.title) {
+            case "Home":
+                displayHomePage();
+                break;
+            case "Our Products":
+                displayProductsPage();
+                break;
+            case "Our Services":
+                displayServicesPage();
+                break;
+            case "About Us":
+                displayAboutPage();
+                break;
+            case "Contact Us":
+                displayContactPage();
+                break;
+        }
+        // Footer Nav bar
+
+        // set the navbar to be fixed to the bottom of the page
+        document.getElementById("footNav").style.position = "fixed";
+        document.getElementById("footNav").style.bottom = "1";
+
+        // set the width and background color of the navbar
+        document.getElementById("footNav").style.width = "100%";
+        document.getElementById("footNav").style.backgroundColor = "#333";
+
+        // set the text color and font size of the copyright statement
+        document.getElementById("footNav").style.color = "#fff";
+        document.getElementById("footNav").style.fontSize = "16px";
+
+        // create and display the current date for the copyright statement
+        const currentDate = new Date();
+        const copyrightStatement = "Copyright " + currentDate.getFullYear();
+        document.getElementById("footNav").innerHTML = copyrightStatement;
+
+        /**********************************
+            Simple DOM Manipulation 3.C) 
+        **********************************/
+        // Get reference to the ul element
+        var navbar = document.querySelector(".navbar-nav");
+
+        // Create the new li element
+        var navLi = document.createElement("li");
+        navLi.classList.add("nav-item");
+
+        // Create the new a element
+        var navLink = document.createElement("a");
+        navLink.classList.add("nav-link");
+        navLink.href = "#";
+        navLink.innerHTML = '<i class="fas fa-user-friends"></i> Human Resources';
+
+        // Append the a element to the li element
+        navLi.appendChild(navLink);
+
+        // Get reference to the About Us li element
+        var aboutUsLi = document.querySelector(".nav-item:nth-child(4)");
+
+        // Insert the new li element after the About Us li element
+        navbar.insertBefore(navLi, aboutUsLi.nextSibling);
+    }
+    window.addEventListener("load", Start);
+    })();
