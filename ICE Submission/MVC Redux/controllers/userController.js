8296f52bc3bd23e55b0870606a8629ad5bdab2d7
Menu-K <menushan.karunakaran@gmail.com>
2023-03-30 11:48:34 -0400

    MVC redux update

--- ICE Submission/MVC Redux/controllers/userController.js
+++ ICE Submission/MVC Redux/controllers/userController.js
@@ -1,3 +1,5 @@
+const { getSWAPI } = require('../helpers/apiCalls');
+
 // home view get
 const homeView = (req, res) => {
     const pageTitle = 'Home';
@@ -7,6 +9,31 @@ const homeView = (req, res) => {
     });
 };
 
+const peopleView = async (req, res) => {
+    const people_url = 'https://swapi.dev/api/people/';
+    let full_people_data = await getSWAPI(people_url);
+    let people_data = full_people_data['results'];
+    // console.log(people_data);
+    for (person in people_data) {
+        let profile = people_data[person];
+        let svgCode = multiavater(profile['name']);
+        profile['avatar'] = svgCode;
+    }
+    res.render('people', { people_data: people_data });
+};
+
+
+
+const planetView = async (req, res) => {
+    const planets_url = 'https://swapi.dev/api/planets/';
+    let full_planets_data = await getSWAPI(planets_url);
+    // console.log(full_planets_data['results']);
+    planets_data = full_planets_data['results'];
+    res.render('planets', { planets_data: planets_data });
+};
+
 module.exports = {
-    homeView
+    homeView,
+    planetView,
+    peopleView
 };
--- ICE Submission/MVC Redux/routes/user.js
+++ ICE Submission/MVC Redux/routes/user.js
@@ -1,9 +1,12 @@
 const express = require('express');
 
-const { homeView } = require('../controllers/userController');
+const { homeView, planetView, peopleView } = require('../controllers/userController');
 
 const router = express.Router();
 
 router.get('/', homeView);
+router.get('/planets', planetView);
+router.get('/people', peopleView);
+
 
 module.exports = router;
