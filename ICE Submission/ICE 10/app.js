689fe446dca63e2da6b3abf70aac9000e7b5ddb2
Menu-K <menushan.karunakaran@gmail.com>
2023-03-30 14:57:10 -0400

    ice 10 update

--- ICE Submission/ICE 10/app.js
+++ ICE Submission/ICE 10/app.js
@@ -1,19 +1,26 @@
+// REQUESTS
 const express = require('express');
 const exphbs = require('express-handlebars');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const path = require('path');
 
+// PRE-APP CONFIG
 dotenv.config({ path: './process.env' });
 
+// CREATE THE APP
 const app = express();
 
+// APP CONFIG
 app.engine('.hbs', exphbs.engine({ defaultLayout: 'main', partialsDir: './views/partials', extname: '.hbs' }));
 app.set('view engine', '.hbs');
 app.set('views', './views');
 
+// ROUTES
+app.use('/', require('./routes/user'));
 
+// SERVER
 const PORT = process.env.PORT;
-app.listen(PORT, () {
+app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
 });
